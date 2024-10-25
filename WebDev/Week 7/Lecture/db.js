52060c61dfbb1cd0ffa30cd9204b9d2b7802bd1d
PrasenKakade21 <prasenkakade21@gmail.com>
2024-10-26 02:18:50 +0530

    Final

--- WebDev/Week 7/Lecture/db.js
+++ WebDev/Week 7/Lecture/db.js
@@ -1,24 +1,43 @@
 const mongoose = require("mongoose");
+const { number, string } = require("zod");
 
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
 const User = new Schema({
-  name: String,
-  email: {type: String, unique: true},
+  Username: String,
+  phone: String,
+  email: {type:String,unique:true},
+  password: String
+});
+const Admin = new Schema({
+  Username: String,
+  phone: String,
+  email: {type:String,unique:true},
   password: String
 });
 
-const Todo = new Schema({
-    userId: ObjectId,
+const Course = new Schema({
+    creatorId: ObjectId,
     title: String,
-    done: Boolean
+    decsription: String,
+    price:Number,
+    imageUrl: String,
+});
+const Purchase = new Schema({
+    userId: ObjectId,
+    courseId: String,
+    note:String
 });
 
 const UserModel = mongoose.model('users', User);
-const TodoModel = mongoose.model('todos', Todo);
+const AdminModel = mongoose.model('admin', Admin);
+const CourseModel = mongoose.model('course', Course);
+const PurchaseModel = mongoose.model('purchase', Purchase);
 
 module.exports = {
     UserModel,
-    TodoModel
+    AdminModel,
+    CourseModel,
+    PurchaseModel
 }
